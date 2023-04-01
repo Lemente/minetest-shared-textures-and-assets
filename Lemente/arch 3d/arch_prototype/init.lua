557c0aafadb96b3036a98a56c4f671b36b3c8f66
Lemente <crafted.by.lemente@gmail.com>
2023-04-01 16:41:38 +0200

    add arch + beam 3d models + mod prototype

---
+++ Lemente/arch 3d/arch_prototype/init.lua
@@ -0,0 +1,47 @@
+local function rotate_and_place(itemstack, placer, pointed_thing)
+	local p0 = pointed_thing.under
+	local p1 = pointed_thing.above
+	local param2 = 0
+
+	if placer then
+		local placer_pos = placer:get_pos()
+		if placer_pos then
+			param2 = minetest.dir_to_facedir(vector.subtract(p1, placer_pos))
+		end
+
+		local finepos = minetest.pointed_thing_to_face_pos(placer, pointed_thing)
+		local fpos = finepos.y % 1
+
+		if p0.y - 1 == p1.y or (fpos > 0 and fpos < 0.5)
+				or (fpos < -0.5 and fpos > -0.999999999) then
+			param2 = param2 + 20
+			if param2 == 21 then
+				param2 = 23
+			elseif param2 == 23 then
+				param2 = 21
+			end
+		end
+	end
+	return minetest.item_place(itemstack, placer, pointed_thing, param2)
+end
+
+for i = 1,18 do
+	minetest.register_node("arch_prototype:"..i, {
+	    drawtype = "mesh",
+		description = "arch piece "..i,
+	    -- Holds the texture for each "material"
+    	tiles = {"arch.png"},
+
+	    -- Path to the mesh
+	    mesh = i..".obj",
+	    groups = {cracky=3,oddly_breakable_by_hand=3,not_in_creative_inventory=0},
+	    paramtype = 'light',paramtype2 = 'facedir',
+	    on_place = function(itemstack, placer, pointed_thing)
+			if pointed_thing.type ~= "node" then
+				return itemstack
+			end
+
+			return rotate_and_place(itemstack, placer, pointed_thing)
+		end,
+	})
+end
